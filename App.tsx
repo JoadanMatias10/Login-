f85b2b841df133e77f9edb95a0dfb943ca39c255
JoadanMatias10 <joadanvidal@gmail.com>
2025-02-06 19:57:22 -0600

    primer login

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import Login from './Aplicacion/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <ActivityIndicator></ActivityIndicator>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
